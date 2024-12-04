e5c69bb45f7cc0bcf05c2ed1d1505ecdc1ef4d4c
Dksony7 <riyanshsony8@gmail.com>
2024-12-05 00:06:34 +0545

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,2 @@
 # app/__init__.py
-from flask import Flask
-from .database import init_db
-
-# Flask app initialization
-app = Flask(__name__)
-
-# Database initialization
-init_db(app)
+# This makes the `app` directory a package
