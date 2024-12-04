9e78c580e1102ed8f3ba19ff3c8a8c4ad13d14d3
Dksony7 <riyanshsony8@gmail.com>
2024-12-05 00:33:39 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes import router
 import uvicorn
+import os
 
 app = FastAPI()
 
@@ -8,4 +9,7 @@ app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    # Use the PORT environment variable if available, default to 8000
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
