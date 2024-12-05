e345a3b732557564af76ddd446551d8a6bf3b597
Dksony7 <riyanshsony8@gmail.com>
2024-12-05 18:21:53 +0545

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,15 +1,18 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+# MongoDB URI
 uri = "mongodb+srv://<db_username>:<db_password>@cluster0.dajxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
+# Specify the database name
+db = client['door_inventory']  # Replace 'door_inventory' with your database name
+
+# Test the connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e)
+    print(f"Failed to connect to MongoDB: {e}")
