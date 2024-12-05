ddb0ec952d211541e5341ef9c44ff8a203b5b1df
Dksony7 <riyanshsony8@gmail.com>
2024-12-05 20:50:13 +0545

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 # MongoDB URI
-uri = "mongodb+srv://<db_username>:<db_password>@cluster0.dajxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://<db_username>:<db_password>@cluster0.dajxp.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
