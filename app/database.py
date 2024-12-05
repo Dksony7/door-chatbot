ee03c9781694b33d16dfa68f0971027c417cdf1d
Dksony7 <riyanshsony8@gmail.com>
2024-12-05 16:48:51 +0545

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # MongoDB Atlas connection
 client = MongoClient("mongodb+srv://Dipika:9812009386@cluster0.dajxp.mongodb.net/?retryWrites=true&w=majority")
-db = client. door-chatbot
+db = client.door-chatbot
 
 # Check connection
 if db:
