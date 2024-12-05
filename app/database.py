e5254ce7e136aba930c54cfcab9657fdd61c578f
Dksony7 <riyanshsony8@gmail.com>
2024-12-05 17:59:32 +0545

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # MongoDB Atlas connection
 client = MongoClient("mongodb+srv://Dipika:9812009386@cluster0.dajxp.mongodb.net/?retryWrites=true&w=majority")
-db = client.PROJECT 0
+db = client.door_inventory
 
 # Check connection
 if db:
