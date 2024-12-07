6c601c16f6991ae51fd0d80903bd482a1d3d336a
Dksony7 <riyanshsony8@gmail.com>
2024-12-07 23:52:57 +0545

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,7 +1,7 @@
 # Sample data model
 default_doors = [
     {"type": "membrane", "design": "membrane", "size": "32x80", "stock": 5, "image_path": "/static/membrane/membrane.jpg"},
-    {"type": "digital", "design": "design1", "size": "32x80", "stock": 1, "image_path": "/static/digital/design1.jpg"},
+    {"type": "digital", "design": "design1", "size": "32x80", "stock": 10, "image_path": "/static/digital/design1.jpg"},
     {"type": "digital", "design": "design2", "size": "32x80", "stock": 3, "image_path": "/static/digital/design2.jpg"},
     {"type": "digital", "design": "design3", "size": "32x80", "stock": 3, "image_path": "/static/digital/design3.jpg"},
     {"type": "warranty", "design": "warranty", "size": "32x80", "stock": 3, "image_path": "/static/warranty/warranty.jpg"},
