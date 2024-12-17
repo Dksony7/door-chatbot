c1c14818490c0ba99926c8de6b69e9923a596206
Dksony7 <riyanshsony8@gmail.com>
2024-12-17 23:30:55 +0545

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,10 +2,14 @@ from pymongo import MongoClient
 
 # MongoDB Atlas connection
 client = MongoClient("mongodb+srv://Dipika:9812009386@cluster0.dajxp.mongodb.net/?retryWrites=true&w=majority")
-db = client.doors
+db = client.doors  # Specify your database name
 
 # Check connection
-if db is not None:  # Change yahan kiya gaya hai
+if db is not None:
     print("Connected to MongoDB Atlas.")
 else:
     print("Failed to connect to MongoDB.")
+
+def get_collection():
+    # Return the MongoDB collection you want to use
+    return db.doors  # Specify your collection name here
