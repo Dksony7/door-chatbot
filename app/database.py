1e2744876bbdc9a76994a33c036feead6d194a7b
Dksony7 <riyanshsony8@gmail.com>
2024-12-18 19:37:54 +0545

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,9 +3,12 @@ from pymongo import MongoClient
 # MongoDB Atlas connection
 try:
     client = MongoClient("mongodb+srv://Dipika:9812009386@cluster0.dajxp.mongodb.net/?retryWrites=true&w=majority")
-    db = client.doors
-    # Test the connection by listing collections
-    collections = db.list_collection_names()
-    print("Connected to MongoDB Atlas. Collections:", collections)
+    db = client['doors']  # Database name
+    collection = db['doors']  # Collection name
+
+    # Fetch stock data
+    stock_data = collection.find()
+    for door in stock_data:
+        print(f"Type: {door['type']}, Size: {door['size']}, Design: {door['design']}, Stock: {door['stock']}, Path: {door['image_path']}")
 except Exception as e:
-    print("Failed to connect to MongoDB:", e)
+    print("Error in fetching stock:", e)
