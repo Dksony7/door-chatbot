8ef21c670ae0fcea406c671fbfe5bdab94b8b455
Dksony7 <riyanshsony8@gmail.com>
2025-01-05 00:13:12 +0545

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
 
 # MongoDB Atlas connection
-try:
-    client = MongoClient("mongodb+srv://Dipika:9812009386@cluster0.dajxp.mongodb.net/?retryWrites=true&w=majority")
-    db = client["doors"]  # Database
+client = MongoClient("mongodb+srv://Dipika:9812009386@cluster0.dajxp.mongodb.net/?retryWrites=true&w=majority")
+db = client.doors
+
+# Check connection
+if db:
     print("Connected to MongoDB Atlas.")
-except Exception as e:
-    print("Failed to connect to MongoDB:", str(e))
+else:
+    print("Failed to connect to MongoDB.")
