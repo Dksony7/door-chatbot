ba2045ba71f325c0e9580eb47d25f0fe7505c63c
Dksony7 <riyanshsony8@gmail.com>
2025-01-04 23:32:54 +0545

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from app.routes import router, static_files
 import os
 import uvicorn
 
+# Initialize FastAPI app
 app = FastAPI()
 
 # Mount static files
 app.mount("/static", static_files, name="static")
 
-# Include router for API and template handling
+# Include API routes
 app.include_router(router)
 
 if __name__ == "__main__":
-    # Get the port from the environment variable or default to 10000
+    # Get port from environment variable or default to 10000
     port = int(os.getenv("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
